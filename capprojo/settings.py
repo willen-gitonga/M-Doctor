92e1c813e38bf380a3b5c534a7e6e41cacd5ae54
savannah8 <willengitonga.wg@gmail.com>
2019-07-11 15:28:47 +0300

    generated the models

--- capprojo/settings.py
+++ capprojo/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chemoapp',
-    'django.contrib.sites',
     'bootstrap4',
     'materialize',
 ]
--- chemoapp/admin.py
+++ chemoapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Doctor)
+admin.site.register(DoctorSpeciality)
+admin.site.register(LiveChat)
+admin.site.register(Patient)
+admin.site.register(Location)
+admin.site.register(DoctorWorkingHours)
+admin.site.register(Appointment)
+admin.site.register(AppointmentDetails)
